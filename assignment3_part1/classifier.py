96da83da4439a7401f27b499c668f608b1c8818c
Yang, Aiden <yunfany3@illinois.edu>
2022-03-26 23:35:37 +0800

    custom model from Alexnet

--- assignment3_part1/classifier.py
+++ assignment3_part1/classifier.py
@@ -34,21 +34,29 @@ class Classifier(nn.Module):
     # TODO: implement me
     def __init__(self):
         super(Classifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5)
-        self.conv2 = nn.Conv2d(64, 32, 3)
-        self.conv3 = nn.Conv2d(32, 16, 3)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 26 * 26, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, NUM_CLASSES)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.fc1 = nn.Linear(256*6*6, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 21)
+
         return
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(x.size()[0], 16 * 26 * 26)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.pool1(self.conv1(x))
+        x = self.pool2(self.conv2(x))
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.pool3(self.conv5(x))
+        x = x.view(-1, 256*6*6)
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.fc3(x)
+
         return x
